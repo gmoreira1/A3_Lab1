2ea0583446e566d7943da9465ae39273b7796b94
Moreira <g_moreira@tss-hr420-dm07.fc.ca>
2017-02-08 13:46:45 -0500

    sdfgh

--- includes/ajaxQuery.php
+++ includes/ajaxQuery.php
@@ -15,7 +15,9 @@
 	$myQuery = "SELECT * FROM mainmodel WHERE model = '$modelNo'";
 	$result = mysqli_query($mysqli, $myQuery);
 	$row = mysqli_fetch_assoc($result);	
-
+	//$_POST = ($modelNo);
+	//$_POST = [$mainmodel];
+	//$_POST = ($id);
 	echo json_encode($row);
 	//echo mysqli_num_rows($result);			
 ?>
--- js/master.js
+++ js/master.js
@@ -1,4 +1,39 @@
 // JavaScript Document
+(function() {
+	"use strict";
+var div = document.querySelector('#cars');
+var images = div.querySelectorAll('.target');
+var modelId = document.querySelector('.click-header');
 
-var box = querySelectorAll('');
-var box2 = querySeclector('');
+function makeRequest() {
+		httpRequest = new XMLHttpRequest();
+
+		if (!httpRequest) {
+			console.log('right');
+			return false;
+		}
+
+		httpRequest.onreadystatechange = mainmodel;
+		httpRequest.open('GET', 'includes/ajaxQuery.php' + '?model=' + this.id);
+		httpRequest.send();
+	}
+	
+	function mainmodel() {
+		if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+			//parse stringified result
+			var modelData = JSON.parse(httpRequest.responseText);
+			modelId.firstChild.nodeValue = modelData.modelName;
+		}
+	}
+			//[].forEach.call(document.querySelectorAll('.hidden'), function(item) {
+				//item.classList.remove('hidden');
+			//});
+
+
+
+
+[].forEach.call(images, function(images) {
+		images.addEventListener('click', makeRequest, false);
+	});
+	
+})();
