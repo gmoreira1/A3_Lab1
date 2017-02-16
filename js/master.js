4c84de12c3bd2bed30d1222d54f4cf41db0628fa
gmoreira1 <g_moreira@fanshaweonline.ca>
2017-02-16 00:03:45 -0500

    Update master.js

--- js/master.js
+++ js/master.js
@@ -1,39 +1,11 @@
 // JavaScript Document
 (function() {
-	"use strict";
-var div = document.querySelector('#cars');
-var images = div.querySelectorAll('.target');
-var modelId = document.querySelector('.click-header');
-
-function makeRequest() {
-		httpRequest = new XMLHttpRequest();
-
-		if (!httpRequest) {
-			console.log('right');
-			return false;
-		}
-
-		httpRequest.onreadystatechange = mainmodel;
-		httpRequest.open('GET', 'includes/ajaxQuery.php' + '?model=' + this.id);
-		httpRequest.send();
-	}
-	
-	function mainmodel() {
-		if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-			//parse stringified result
-			var modelData = JSON.parse(httpRequest.responseText);
-			modelId.firstChild.nodeValue = modelData.modelName;
-		}
-	}
-			//[].forEach.call(document.querySelectorAll('.hidden'), function(item) {
-				//item.classList.remove('hidden');
-			//});
-
-
-
-
-[].forEach.call(images, function(images) {
-		images.addEventListener('click', makeRequest, false);
+	$('.thumbInfo img').on('click', function() {
+		
+	$.getJSON('includes/ajaxQuery.php', { model:this.id }, function(data){
+	$('.modelName').text(data.modelName);
+	$('.priceInfo').text(data.pricing);
+	$('.modelDetails').text(data.modelDetails);
 	});
-	
+	});
 })();
